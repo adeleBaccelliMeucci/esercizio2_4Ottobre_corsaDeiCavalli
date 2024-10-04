e14160adf8c4a498e372fd4c97069bec2a89066a
lai2-24 <lai2-24@itismeucci.com>
2024-10-04 10:36:36 +0200

    finito

--- src/main/java/com/example/Cavallo.java
+++ src/main/java/com/example/Cavallo.java
@@ -1,7 +1,8 @@
 package com.example;
 
-public class Cavallo extends Thread{
+public class Cavallo extends Thread implements Comparable<Cavallo>{ 
 
+    private String nome;
     private int distanza;
     private static int t = 0; //tempo impiegato
     private int tempoImpiegato;
@@ -10,7 +11,8 @@ public class Cavallo extends Thread{
         return tempoImpiegato;
     }
 
-    public Cavallo (int distanza){ //distanza da fare
+    public Cavallo (int distanza, String nome){ //distanza da fare
+        this.nome = nome;
         this.distanza = distanza;
         this.tempoImpiegato = 0;
     }
@@ -20,5 +22,15 @@ public class Cavallo extends Thread{
             t++;
         }
         tempoImpiegato = t;
+    }
+
+    @Override
+    public String toString(){
+        return "nome: " + nome + ". Tempo impiegato: " + tempoImpiegato; //tutti i dati
+    }
+
+    @Override
+    public int compareTo(Cavallo c){
+        return this.tempoImpiegato - c.getTempoImpiegato();
     }
 }
--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,13 +1,38 @@
 package com.example;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        
+        int percorso = 100; //distanza
+        int numeroCavalli = 20;
+        
         System.out.println("INIZIO");
 
-        
+        ArrayList<Cavallo> gara = new ArrayList<>();
+        for(int i=0; i<numeroCavalli; i++){
+            gara.add(new Cavallo(percorso, "cavallo" + i));
+        }
 
+        for(int i=0; i<numeroCavalli; i++){//faccio partire tutti
+            gara.get(i).start();
+        }
+
+        for(int i=0; i<numeroCavalli; i++){//faccio fermare tutti
+            gara.get(i).join();
+        }
+
+        ArrayList<Cavallo> podio = new ArrayList<>();
+        podio = gara;//per essere più comodo //copia superficiale
+        
+        Collections.sort(podio);
+
+        for (int i = 0; i < podio.size(); i++) { //output dati
+            System.out.println("Posizione: " + (i+1) + podio.get(i).toString() );
+        }
 
     }
 }
