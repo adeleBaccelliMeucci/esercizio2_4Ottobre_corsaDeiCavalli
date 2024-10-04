69dac39e0e86b190d9793cc136a1442568360ecf
lai2-24 <lai2-24@itismeucci.com>
2024-10-04 09:46:55 +0200

    inizio

---
+++ src/main/java/com/example/Cavallo.java
@@ -0,0 +1,18 @@
+package com.example;
+
+public class Cavallo extends Thread{
+
+    private int distanza;
+    private static int t = 0; //tempo impiegato
+
+    public Cavallo (int distanza){ //distanza da fare
+        this.distanza = distanza;
+    }
+
+    public void run(){
+        for(int i = 0; i<distanza ; i++){
+            
+        }
+        
+    }
+}
